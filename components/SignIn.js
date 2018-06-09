f33bd1808e1440976dae9e6251b0310818e4d767
David Ernst <git@dsernst.com>
2018-06-08 19:55:57 -0700

    HOTFIX: Revert request_email_totp arguments

--- components/SignIn.js
+++ components/SignIn.js
@@ -29,7 +29,7 @@ module.exports = class SignIn extends Component {
 
     return this.api(`/rpc/request_email_totp`, {
       method: 'POST',
-      body: JSON.stringify({ email, phone_user_id, device_desc: this.location.userAgent || 'Unknown', signup_channel: 'united.vote' }),
+      body: JSON.stringify({ email, device_desc: this.location.userAgent || 'Unknown' }),
     })
     .then(({ device_secret, jwt, refresh_token, user_id }) => {
       const oneYearFromNow = new Date(Date.now() + (365 * 24 * 60 * 60 * 1000))
