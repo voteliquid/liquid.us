e9873ed610b34cfd7fbdf42745bae40bfe044ce1
Alex Mingoia <talk@alexmingoia.com>
2018-09-25 10:59:09 -0700

    Remove max-width on summary when expanded.

--- components/MeasureDetails.js
+++ components/MeasureDetails.js
@@ -95,7 +95,7 @@ class MeasureSummary extends Component {
           left: 0;
         }
       </style>
-      <div class=${`${expanded || !summary ? '' : 'summary'}`} style=${summary && summary.length > 512 ? 'max-height: 10rem;' : ''}>
+      <div class=${`${expanded || !summary ? '' : 'summary'}`} style=${!expanded && summary && summary.length > 512 ? 'max-height: 10rem;' : ''}>
         <div class="content">
           ${[summary ? summary.replace(/\n/g, '<br />') : `<p>A summary is in progress. <a href="https://www.congress.gov/bill/${congress}th-congress/${chamber === 'Lower' ? 'house' : 'senate'}-bill/${number}/text" target="_blank">Read full text of the bill at congress.gov <span class="icon is-small"><i class="fa fa-external-link"></i></span></a>`]}
         </div>
