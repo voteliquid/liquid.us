4852986a3d0047ace71a646fbadb2bc97a40b69c
Alex Mingoia <talk@alexmingoia.com>
2018-09-24 21:46:50 -0700

    Initialize Twilio when requested.

--- middleware/verify_phone_number.js
+++ middleware/verify_phone_number.js
@@ -1,7 +1,8 @@
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_KEY } = process.env
-const twilio = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_KEY)
 
 module.exports = (req, res) => {
+  const twilio = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_KEY)
+
   const phone_number = req.body.phone_number
   twilio.lookups.phoneNumbers(`+1${phone_number}`)
     .fetch({ countryCode: 'US' })
