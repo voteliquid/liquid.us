872fa2c79e98e9ddd9f5f3eb9d5ad153c31060a9
Alex Mingoia <talk@alexmingoia.com>
2018-10-23 11:40:40 -0700

    Remove Google Translate

--- components/HtmlWrapper.js
+++ components/HtmlWrapper.js
@@ -89,13 +89,6 @@ module.exports = (state, html, bundleUrl) => {
 
           window.__lo_site_id = 119200;
         </script>
-        <div id="google_translate_element" style="position: fixed; bottom: 0; right: 100px; z-index: 99;"></div>
-        <script type="text/javascript">
-          function googleTranslateElementInit() {
-            new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true, gaId: 'UA-84279342-5'}, 'google_translate_element');
-          }
-        </script>
-        <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
         <div>
           ${[NODE_ENV === 'production' ? `
             <script async src="https://d10lpsik1i8c69.cloudfront.net/w.js"></script>
