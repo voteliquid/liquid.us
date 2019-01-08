7e40a24c47365828d05582667efc8db2b847798b
Alex Mingoia <talk@alexmingoia.com>
2019-01-08 15:04:04 -0800

    Remove rep letter grades.

--- components/RepCard.js
+++ components/RepCard.js
@@ -2,6 +2,7 @@ const { avatarURL, html } = require('../helpers')
 const ordinalSuffix = require('ordinal-suffix')
 
 module.exports = ({ rep, office }) => {
+  const with_constituent_percentage = rep.constituent_vote_count ? Math.floor((rep.with_constituent_vote_count / rep.constituent_vote_count) * 100) : 0
   return html(`repcard-${rep.user_id}`)`
     <div class="media">
       <figure class="media-left">
@@ -29,12 +30,20 @@ module.exports = ({ rep, office }) => {
             <span class="is-size-6">${office.name}</span>
           </p>
           <p>
-            <span class="tag is-size-6 is-dark has-text-weight-bold">${rep.representation_grade || [`<span class="icon"><i class="fa fa-question"></i></span>`]}</span>
-            <span class="is-size-7 has-text-grey">
-              ${rep.representation_grade ?
-                `${ordinalSuffix(rep.representation_percentile)} percentile among ${office.chamber === 'Lower' ? 'House' : 'Senate'} ${rep.party_affiliation}s` :
-                `Need more constituent votes to calculate grade`}
-            </span>
+            ${rep.representation_percentile && rep.constituent_vote_count ?
+            [`
+              <span class="has-text-weight-bold is-size-6">${rep.with_constituent_vote_count || 0} / ${rep.constituent_vote_count || 0}</span>
+              <span class="is-size-7 has-text-grey">votes with verified constituents in ${rep.elected_office_short_name}</span>
+              <br />
+              <span class="has-text-weight-bold is-size-6">${with_constituent_percentage}%</span>
+              <span class="is-size-7 has-text-grey">aligned with constituents</span>
+              <br />
+              <span class="has-text-weight-bold is-size-6">${ordinalSuffix(rep.representation_percentile)}</span>
+              <span class="is-size-7 has-text-grey">percentile among ${rep.elected_office_chamber === 'Lower' ? 'House' : 'Senate'} ${rep.party_affiliation}s</span>
+            `] : [`
+              <span class="has-text-weight-bold is-size-7"><span class="icon"><i class="fa fa-question"></i></span></span>
+              <span>Need more constituent votes to calculate their scorecard.<span>
+            `]}
           </p>
         </div>
       </div>
