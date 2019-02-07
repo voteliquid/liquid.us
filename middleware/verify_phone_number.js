3b6dc6f09df7e654296f7d268d9b9267d93d8e46
David Ernst <git@dsernst.com>
2019-02-07 08:50:27 -0500

    Fix misnamed TWILIO_AUTH_TOKEN

--- middleware/verify_phone_number.js
+++ middleware/verify_phone_number.js
@@ -1,10 +1,10 @@
-const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_KEY } = process.env
+const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } = process.env
 
 module.exports = (req, res) => {
-  if (!TWILIO_ACCOUNT_SID || !TWILIO_AUTH_KEY) {
+  if (!TWILIO_ACCOUNT_SID || !TWILIO_AUTH_TOKEN) {
     return res.status(400).json({ message: 'Missing phone_verification keys (process.env)' })
   }
-  const twilio = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_KEY)
+  const twilio = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 
   const phone_number = req.body.phone_number
