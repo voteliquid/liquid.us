2106fc5fcd1e3d59e19db8237f351836fdfab530
David Ernst <git@dsernst.com>
2019-02-06 20:06:59 -0500

    Improve error message for missing TWILIO keys

--- middleware/verify_phone_number.js
+++ middleware/verify_phone_number.js
@@ -1,7 +1,11 @@
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_KEY } = process.env
 
 module.exports = (req, res) => {
+  if (!TWILIO_ACCOUNT_SID || !TWILIO_AUTH_KEY) {
+    return res.status(400).json({ message: 'Missing phone_verification keys (process.env)' })
+  }
   const twilio = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_KEY)
+
 
   const phone_number = req.body.phone_number
   twilio.lookups.phoneNumbers(`+1${phone_number}`)
