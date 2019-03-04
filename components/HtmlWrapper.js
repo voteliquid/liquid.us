40cb06ccfcc7257677ab33962f60067264ecc613
Alex Mingoia <talk@alexmingoia.com>
2019-03-04 14:02:56 -0800

    Remove CloudFront script that's meant to be injected into HTML.

--- components/HtmlWrapper.js
+++ components/HtmlWrapper.js
@@ -136,7 +136,6 @@ module.exports = (state, html, bundleUrl) => {
         </script>
         <div>
           ${[NODE_ENV === 'production' ? `
-            <script async src="https://d10lpsik1i8c69.cloudfront.net/w.js"></script>
             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-84279342-5"></script>
             <script>
               window.dataLayer = window.dataLayer || [];
