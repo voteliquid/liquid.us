d61b9ff577280d9de9e5561b23307afd6a990eb7
Alex Mingoia <talk@alexmingoia.com>
2019-04-29 21:44:40 -0700

    Use correct responsive video wrapper classes on homepage.

--- views/video.js
+++ views/video.js
@@ -2,8 +2,8 @@ const { html } = require('../helpers')
 
 module.exports = ({ url }) => {
   return html`
-    <div style="max-width: 600px; margin: 50px auto 0">
-      <div class="responsive-video-wrapper reveal">
+    <div class="responsive-video-outer">
+      <div class="responsive-video-inner reveal">
         <iframe width="560" height="315" src="${url}" frameborder="0" allowfullscreen></iframe>
       </div>
     </div>
