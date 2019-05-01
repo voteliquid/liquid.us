30daa24773ea1f1d21a52ba9e3c0a451f106fd8e
David Ernst <git@dsernst.com>
2019-04-30 23:53:46 -0700

    Shrink percentile number on Rep profile page

--- views/legislator-scorecard.js
+++ views/legislator-scorecard.js
@@ -20,7 +20,7 @@ module.exports = (state) => {
             <span class="has-text-weight-bold has-text-grey-dark is-size-5">${with_constituent_percentage}% </span>
             <span class="is-size-6 has-text-grey">aligned with constituents</span>
             <br />
-            <span class="has-text-weight-bold has-text-grey-dark is-size-5">${ordinalSuffix(representation_percentile)} </span>
+            <span class="has-text-weight-bold has-text-grey-dark is-size-6">${ordinalSuffix(representation_percentile)} </span>
             <span class="is-size-6 has-text-grey">percentile among ${elected_office_chamber === 'Lower' ? 'House' : 'Senate'} ${party_affiliation}s</span>
           ` : html`
             <span class="has-text-weight-bold has-text-grey-dark is-size-5"><span class="icon"><i class="fa fa-question"></i></span></span>
