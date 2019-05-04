a830ef38b8e703e934189c80d71e2d44c7ad23eb
David Ernst <git@dsernst.com>
2019-05-03 17:49:04 -0700

    Only show EndorsementCallReps if we have at least 1 phone number

--- views/endorsement-phone-reps.js
+++ views/endorsement-phone-reps.js
@@ -3,14 +3,20 @@ const { avatarURL, html } = require('../helpers')
 module.exports = ({ measure, reps }) => {
 
   const targetReps = reps.filter(r =>
-    r.legislature.short_name === measure.legislature_name
-    || r.legislature.name === measure.legislature_name
+    [r.legislature.short_name, r.legislature.name].includes(measure.legislature_name)
   )
+  .map(r => ({ ...r, // TODO: Temp code to add placeholder phone numbers for CA Reps
+    // Remove this map function once we get phone numbers from the API
+    phone: r.legislature.name === 'CA' ? '5109876543' : null
+  }))
+  .filter(r => r.phone)
 
   return html`
     <div class="content">
-      <p class="title is-4 has-text-centered" style="margin-bottom: 5px;">Call Your Reps</p>
-      ${targetReps.map(rep)}
+      ${targetReps.length ? html`
+        <p class="title is-4 has-text-centered" style="margin-bottom: 5px;">Call Your Reps</p>
+        ${targetReps.map(rep)}
+      ` : ''}
     </div>
   `
 }
@@ -21,10 +27,7 @@ const rep = (r) => {
   const nameLine = isState
     ? `${rep.first_name} ${rep.last_name}, ${r.legislature.short_name}`
     : `${r.chamber === 'Upper' ? 'Sen' : 'Rep'}. ${rep.first_name} ${rep.last_name}`
-  const num = '5109876543'
-  function displayNum(num) {
-    return `(${num.slice(0, 3)}) ${num.slice(3, 6)}-${num.slice(6)}`
-  }
+  const displayNum = (phone) => `(${phone.slice(0, 3)}) ${phone.slice(3, 6)}-${phone.slice(6)}`
 
   return html.for(r, `call-rep-${r.id}`)`
     <div class="column is-narrow">
@@ -36,7 +39,7 @@ const rep = (r) => {
         </div>
         <div class="media-content has-text-weight-semibold is-size-5" style="line-height: 24px;">
           ${nameLine}<br />
-          <a class="button is-success is-small" src=${`tel:+1${num}`}>${displayNum(num)}</a>
+          <a class="button is-success is-small" src=${`tel:+1${r.phone}`}>${displayNum(r.phone)}</a>
         </div>
       </div>
     </div>
