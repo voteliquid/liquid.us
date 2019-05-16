343d890e650ce0d7718c2b4c4f56f31875ad0119
David Ernst <git@dsernst.com>
2019-05-16 09:35:02 -0700

    Show grey background behind centered videos

--- helpers.js
+++ helpers.js
@@ -103,7 +103,7 @@ exports.linkifyUrls = (text = '') => {
           url = `https://media.cityofmadison.com/Mediasite/Play/${videoMatch[6]}`
         }
         return `
-          <div class="responsive-video-outer" style="margin: 0 auto;"><div class="responsive-video-inner"><iframe width="560" height="315" src="${url}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div></div>
+          <div style="max-width: 100%; background: hsla(0, 0%, 87%, 0.25);"><div class="responsive-video-outer" style="margin: 0 auto;"><div class="responsive-video-inner"><iframe width="560" height="315" src="${url}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div></div></div>
         `
       }
       if (url.match(/\.(png|jpg|jpeg|gif)$/i)) {
