7ff51077067d379983bfe2135e3599dd653a8530
dallasjc <39286778+dallasjc@users.noreply.github.com>
2019-05-23 08:09:04 -0500

    Moves check for Dane County outside of html

--- views/measure-sidebar.js
+++ views/measure-sidebar.js
@@ -164,6 +164,7 @@ const measureVoteCounts = ({ measure, offices }) => {
     'U.S. Congress': { Upper: 'Senate', Lower: 'House' },
     'CA': { Upper: 'Senate', Lower: 'Assembly' },
   }
+  const wiNotDane = measure.legislature_name === 'Dane County' ? 'WI' : ''
 
   return html`
     <div class="panel-block">
@@ -195,7 +196,7 @@ const measureVoteCounts = ({ measure, offices }) => {
               <td class="has-text-right">Nay</td>
             </tr>
             <tr>
-              <td class="has-text-left has-text-grey">${legislature_name === 'Dane County' ? 'WI Total' : legislature_name.replace(' Congress', '')}</td>
+              <td class="has-text-left has-text-grey">${wiNotDane || legislature_name.replace(' Congress', '')}</td>
               <td class="has-text-right">${yeas || 0}</td>
               <td class="has-text-right">${nays || 0}</td>
             </tr>
