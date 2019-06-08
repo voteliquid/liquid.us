d8e6c6de5041f85854c691ff737b33f83bf71277
dallasjc <39286778+dallasjc@users.noreply.github.com>
2019-06-08 14:22:27 -0500

    Add measure image checks for determining social on vote pages

--- models/vote.js
+++ models/vote.js
@@ -394,14 +394,22 @@ const endorsementPageTitleAndMeta = (measures, vote, location) => {
     .split('\n').filter(line => line)[0] // strip leading whitespace
   const title = `${vote.fullname || anonymousName}: ${firstRealSentence}`
 
+
   const inlineImageMatch = vote && vote.comment.match(/\bhttps?:\/\/\S+\.(png|jpg|jpeg|gif)\b/i)
   const inlineImage = inlineImageMatch && inlineImageMatch[0]
+  const measureInlineImageMatch = measure && measure.summary && measure.summary.match(/\bhttps?:\/\/\S+\.(png|jpg|jpeg|gif)\b/i)
+  const measureInlineImage = measureInlineImageMatch && measureInlineImageMatch[0]  const billAuthorsComment = measure.image_name && measure.author_username === vote.username
+    ? `${ASSETS_URL}/measure-images/${measure.image_name}`
+    : measure.author_username === vote.username && measureInlineImageMatch
+    ? measureInlineImageMatch[0]
+    : ''
   const authorImage = vote.username || vote.twitter_username ? avatarURL(vote) : null
-  const measureImage = (!isCity) ? `${ASSETS_URL}/legislature-images/${measure.legislature_name}.png` : ''
+  const dbMeasureImage = measure.image_name ? `${ASSETS_URL}/measure-images/${measure.image_name}` : ''
+  const legislatureImage = (!isCity) ? `${ASSETS_URL}/legislature-images/${measure.legislature_name}.png` : ''
 
   return {
     ...location,
-    ogImage: inlineImage || authorImage || measureImage,
+    ogImage: inlineImage || billAuthorsComment || authorImage || dbMeasureImage || measureInlineImage || legislatureImage,
     ogTitle: `${measure.title} | Liquid US`,
     description: title,
     title,
