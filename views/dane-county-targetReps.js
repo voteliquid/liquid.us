a04e337a9554e5bf115030226fff1728b7d9c731
dallasjc <39286778+dallasjc@users.noreply.github.com>
2019-06-12 10:58:47 -0500

    Updates call-to-action now that jail approved

--- views/dane-county-targetReps.js
+++ views/dane-county-targetReps.js
@@ -1,9 +1,30 @@
 const { ASSETS_URL } = process.env
 const { html } = require('../helpers')
 
-module.exports = ({ vote, user }) => {
+module.exports = ({ vote, user, reps }) => {
   const measureImage = `${ASSETS_URL}/legislature-images/Dane County.png`
   const reply = (vote.replies || []).filter(({ user_id }) => (user && user.id === user_id))[0]
+  const supervisor = reps[4].office_holder
+  const last = (arr) => arr[arr.length - 1]
+  const district = last(supervisor.elected_office_name.split(' '))
+  const emailExceptions = {
+  'Dane7': 'veldran@countyofdane.com', // Veldran
+  'Dane8': 'bayrd@countyofdane.com', // Bayrd
+  'Dane12': 'rusk@countyofdane.com', // Rusk
+  'Dane19': 'clausius@countyofdane.com', // Clausius
+  'Dane23': 'stubbs@countyofdane.com', // Stubbs
+  'Dane30': 'downing@countyofdane.com', // Downing
+  'Dane34': 'miles@countyofdane.com', // Miles
+  'Dane37': 'salov@countyofdane.com', // Salov
+  'Dane25': 'kiefer.timothy@countyofdane.com', // Kiefer
+}
+const email = emailExceptions[supervisor.short_name] || `${supervisor.last_name.toLowerCase()}.${supervisor.first_name.toLowerCase()}@countyofdane.gov`
+const noSup = !supervisor || !supervisor.first_name || supervisor.first_name === 'Vacant'
+const supSupportsProDane = ['2', '4', '6', '32'].includes(district)
+const supSemiSupports = ['35', '27'].includes(district)
+console.log(email)
+
+console.log(reps[4].office_holder)
   return html`
     <br />
     <div class="columns">
@@ -26,12 +47,20 @@ module.exports = ({ vote, user }) => {
     </div>
     ${reply ? html`
       <div class="is-size-5 box">
-        <p>Your comment will be sent to your legislators, but you should email them directly at <a href="mailto:county_board_recipients@countyofdane.com" target="_blank">county_board_recipients@countyofdane.com</a> or attend the June 6th Board meeting to emphasize the importance of this issue:</p>
+        <p>Thank you for supporting Resolution 67, the Community Alternative Plan for Housing, Health care, and Decarceration.</p><br />
+        ${noSup ? html`
+          <p>Your supervisor was recently elected and has not yet been sworn in. We will send your comment when that happens. In the meantime, contact the Board of Supervisors at <a href="mailto:county_board_recipients@countyofdane.com" target="_blank">county_board_recipients@countyofdane.com</a> to emphasize the importance of this issue.</p>
+        ` : supSupportsProDane ? html`
+          <p>Your supervisor voted against the jail and already supports Resolution 67. Send Supervisor ${supervisor.last_name} a quick <a href="${`mailto:${email}`}" target="_blank">thank you email</a>.</p>
+        ` : supSemiSupports ? html`
+          <p>Your supervisor supports OA-3, a measure that would strengthen the Criminal Justice Court, but has so far declined to support Resolution 67. Send a quick email thanking Supervisor ${supervisor.last_name} for supporting 0A-3, but explain that it's not enough: <a href="mailto:${email}" target="_blank">${email}</a>.</p>
+      ` : html`
+        <p>Your supervisor voted for the jail and has declined to endorse Resolution 67. Please reach out to Supervisor ${supervisor.last_name} at <a href="mailto:${email}" target="_blank">${email}</a> to explain why you believe it is so important to prioritize decarceration.
+      `}
         <br />
-        <p><a href="https://goo.gl/maps/kQWbRGMjhS6sMpuXA" target="_blank">City County Building, 210 MLK Jr Blvd, Madison, WI, 53703</a></p>
-        <p><b>June 6, 2019</b> 7:00pm in room 201, Dane County Board of Supervisors vote</p>
+        <p>It's important that we keep up the pressure after our big turnout last week. Please share this page to help us find even more supporters.</p>
         <br />
-        <p><em>We will provide an update as the meeting dates are confirmed.</em></p>
+        <p><em>The first commmittee hearing should be heald in late June or early July. We will provide an update when the dates are confirmed.</em></p>
       </div>
     ` : ''}
   `
