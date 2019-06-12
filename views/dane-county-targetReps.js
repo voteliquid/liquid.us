fe6d623ce9e2893ad35820a757f3e7951c0452de
dallasjc <39286778+dallasjc@users.noreply.github.com>
2019-06-12 11:10:50 -0500

    Hide emails behind text

--- views/dane-county-targetReps.js
+++ views/dane-county-targetReps.js
@@ -22,9 +22,7 @@ const email = emailExceptions[supervisor.short_name] || `${supervisor.last_name.
 const noSup = !supervisor || !supervisor.first_name || supervisor.first_name === 'Vacant'
 const supSupportsProDane = ['2', '4', '6', '32'].includes(district)
 const supSemiSupports = ['35', '27'].includes(district)
-console.log(email)
 
-console.log(reps[4].office_holder)
   return html`
     <br />
     <div class="columns">
@@ -49,14 +47,14 @@ console.log(reps[4].office_holder)
       <div class="is-size-5 box">
         <p>Thank you for supporting Resolution 67, the Community Alternative Plan for Housing, Health care, and Decarceration.</p><br />
         ${noSup ? html`
-          <p>Your supervisor was recently elected and has not yet been sworn in. We will send your comment when that happens. In the meantime, contact the Board of Supervisors at <a href="mailto:county_board_recipients@countyofdane.com" target="_blank">county_board_recipients@countyofdane.com</a> to emphasize the importance of this issue.</p>
+          <p>Your supervisor was recently elected and has not yet been sworn in. We will send your comment when that happens. In the meantime, <a href="mailto:county_board_recipients@countyofdane.com" target="_blank">email the Board of Supervisors</a> to emphasize the importance of this issue.</p>
         ` : supSupportsProDane ? html`
           <p>Your supervisor voted against the jail and already supports Resolution 67. Send Supervisor ${supervisor.last_name} a quick <a href="${`mailto:${email}`}" target="_blank">thank you email</a>.</p>
         ` : supSemiSupports ? html`
-          <p>Your supervisor supports OA-3, a measure that would strengthen the Criminal Justice Court, but has so far declined to support Resolution 67. Send a quick email thanking Supervisor ${supervisor.last_name} for supporting 0A-3, but explain that it's not enough: <a href="mailto:${email}" target="_blank">${email}</a>.</p>
-      ` : html`
-        <p>Your supervisor voted for the jail and has declined to endorse Resolution 67. Please reach out to Supervisor ${supervisor.last_name} at <a href="mailto:${email}" target="_blank">${email}</a> to explain why you believe it is so important to prioritize decarceration.
-      `}
+          <p>Your supervisor supports OA-3, a measure that would strengthen the Criminal Justice Court, but has so far declined to support Resolution 67. Send a  <a href="mailto:${email}" target="_blank">quick email</a> thanking Supervisor ${supervisor.last_name} for supporting 0A-3, but explain that it's not enough.</p>
+        ` : html`
+          <p>Your supervisor voted for the jail and has declined to endorse Resolution 67. Please <a href="mailto:${email}" target="_blank">reach out</a> to Supervisor ${supervisor.last_name} to explain why you believe it is important to prioritize decarceration.
+        `}
         <br />
         <p>It's important that we keep up the pressure after our big turnout last week. Please share this page to help us find even more supporters.</p>
         <br />
