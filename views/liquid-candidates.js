c2ebb71f982d33a24700024ea0f8b49ceca6836c
dallasjc <39286778+dallasjc@users.noreply.github.com>
2019-07-20 08:39:43 -0500

    Clean up liquid candidate page code

--- views/liquid-candidates.js
+++ views/liquid-candidates.js
@@ -1,40 +1,22 @@
-const { handleForm, html } = require('../helpers')
+const { html, linkifyUrls } = require('../helpers')
 
-module.exports = (state, dispatch) => {
-  const { error } = state
+module.exports = () => {
 
   return html`
     <section class="section">
       <div class="container is-widescreen is-size-5">
         <h2 class="title has-text-weight-normal is-4">Become a Liquid Candidate</h2>
-        <div>
-          <p>American citizens all over the country have ran for office while committing to use digital democracy tools.</p><br />
-          <p> We want to take the next step: elect candidates pledged to follow the will of their constituents as expressed through Liquid US or a similar liquid system.</p><br />
-          <p><b>This means we need liquid supporters like you to run for office.</b></p><br />
-          <p>We'll help you get on the ballot, connect you with other local liquid supporters, and provide as much guidance and support as we can.</p><br />
-          <p>Intrigued? Fill out the form below to get started:</p>
-        </div><br />
-        <form method="POST" onsubmit=${handleForm(dispatch, { type: 'measure:editFormSaved' })}>
-          ${error ? html`<div class="notification is-danger">${error.message}</div>` : ''}
-          <div class="field has-text-centered">
-            <div class="contact-info-box">
-              <input name="name" class="input" type="text" autocomplete="off" placeholder="Name" />
-            </div>
-            <div class="contact-info-box">
-              <input name="email" class="input" type="text" autocomplete="off" placeholder="Email address" />
-            </div>
-            <div class="contact-info-box">
-              <input name="phone" class="input" type="text" autocomplete="off" placeholder="Phone (optional)" />
-            </div>
+        <div class="columns">
+          <div class="column">
+            <p>The path to a liquid democracy starts with liquid candidates. These candidates pledge that, if elected, they will use constituent votes cast through Liquid US or a similar liquid system to determine how to vote on every bill.</p><br />
+            <p><b>That means we need liquid supporters like you run for office.</b></p>
+            <p>${{ html: linkifyUrls('https://youtu.be/X1fiLKO51qY?t=141') }}</p>
+            <p>We'll help you get on the ballot, connect you with other local liquid supporters, and provide as much guidance and support as we can.</p><br />
+            <p>Intrigued? Fill out the form to learn more.</p>
           </div>
-          <div class="field is-grouped">
-            <div class="control submit-button">
-              <button class="button is-primary" type="submit">
-                <span class="icon"><i class="fa fa-edit"></i></span>
-                <span>Submit</span>
-              </button>
-            </div>
-        </form>
+          <div class="column is-one-quarter">
+          </div>
+        </div>
       </div>
       <style>
         @media (min-width: 1050px) {
